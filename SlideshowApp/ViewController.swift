6e733e89a46a2c17a71c7d64f13d9e47c87a0824
shinpei-nonaka <shinpei_nonaka@jrass.jp>
2022-08-27 14:38:52 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by PC-SYSKAI556 on 2022/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
