546ccf6bdffd7addc8b677dc82aa86ccb3f18731
shinpei-nonaka <shinpei_nonaka@jrass.jp>
2022-09-13 10:45:25 +0900

    jgfkjgkjhl

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var beta: UIImageView!
+    let imageName = ["petfish_pefaaaa2015", "photo020_b" ,"c18c85_10a2f59c275c425c866c2a082823c05b_mv2"]
+    var changeImage = 0
+    
+    
+    @IBAction func next(_ sender: Any) {
+    if changeImage == 0 {
+            changeImage = 1
+        }else if changeImage == 1 {
+            changeImage = 2
+        }else if changeImage == 2 {
+            changeImage = 0
+        }
+        let name = imageName[changeImage]
+        beta.image = UIImage(named: name)
+    }
+        
+    @IBAction func back(_ sender: Any) {
+        if changeImage == 0 {
+                changeImage = 2
+            }else if changeImage == 1 {
+                changeImage = 0
+            }else if changeImage == 2 {
+                changeImage = 1
+            }
+            let name = imageName[changeImage]
+            beta.image = UIImage(named: name)
+    }
+    
+    var timer: Timer!
+    
+    @IBAction func slide(_ sender: Any) {
+        if (timer == nil){
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(slideshow), userInfo: nil, repeats: true)
+            startButton.setTitle("停止", for: .normal)
+            nextButton.isEnabled = false
+            backButton.isEnabled = false
+        } else {
+            timer.invalidate()
+            timer = nil
+            startButton.setTitle("再生", for: .normal)
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
+        }
+    }
+    
+    @objc func slideshow(){
+        changeImage += 1
+        if (changeImage == imageName.count){
+            changeImage = 0
+        }
+        let name = imageName[changeImage]
+        beta.image = UIImage(named: name)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        beta.image = UIImage(named: "petfish_pefaaaa2015")
         // Do any additional setup after loading the view.
+        
+        
+        
     }
-
 
 }
 
---
+++ SlideshowApp/nextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  SlideshowApp
+//
+//  Created by PC-SYSKAI556 on 2022/09/06.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
